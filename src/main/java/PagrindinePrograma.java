31ad10eb74eb3c13f7de66d4c4376758c8655b11
saulgiedr1 <saulgiedr@gmail.com>
2022-07-29 15:03:50 +0300

    sutvarkyta ivedimas per konsole visi punktai

--- src/main/java/PagrindinePrograma.java
+++ src/main/java/PagrindinePrograma.java
@@ -12,72 +12,47 @@ public class PagrindinePrograma {
     public static void main(String[] args) {
         Connection jungtis = VeiksmaiSuDuomenuBaze.prisijungtiPrieDuombenuBazesReceptaiDb();
 
-        ArrayList<Receptai> visiReceptai = VeiksmaiSuDuomenuBaze.grazintiVisusReceptus(jungtis);
-        PapildomosFunkcijos.isvestiReceptuListaEilutemis(visiReceptai);
+        int pasirinkimas = KonsolesVeiksmai.meniuPasirinkimai();
 
-        System.out.println("------------------");
-        ArrayList<Receptai> receptuKaina = VeiksmaiSuDuomenuBaze.grazintiKaina(jungtis, 20);
-        System.out.println("Receptai, kuriu kaina yra didesnė už x");
+        switch (pasirinkimas) {
+            case 1:
+                ArrayList<Receptai> visiReceptai = VeiksmaiSuDuomenuBaze.grazintiVisusReceptus(jungtis);
+                PapildomosFunkcijos.isvestiReceptuListaEilutemis(visiReceptai);
+                break;
+            case 2:
+                ArrayList<Ingredientai> visiIngredientai = VeiksmaiSuDuomenuBaze.grazintiVisusIngredientus(jungtis);
+                PapildomosFunkcijos.isvestiIngredientuListaEilutemis(visiIngredientai);
+                break;
 
-        System.out.println("------------------");
-
-        PapildomosFunkcijos.isvestiReceptuListaEilutemis(receptuKaina);
-
-        System.out.println("------------------");
-
-        // Receptai pridetiRecepta = new Receptai("Bestrogenas", 25.69, "Iškepti");
-        // VeiksmaiSuDuomenuBaze.pridetiRecepta(jungtis, pridetiRecepta);
-
-
-        //---------------------------------------------------------
-
-
-        ArrayList<Ingredientai> visiIngredientai = VeiksmaiSuDuomenuBaze.grazintiVisusIngredientus(jungtis);
-        PapildomosFunkcijos.isvestiIngredientuListaEilutemis(visiIngredientai);
-        //----------------------------------------------------------
-
-        System.out.println("------------------");
-        ArrayList<Ingredientai> igredientuKaina = VeiksmaiSuDuomenuBaze.grazintiIngredientoKaina(jungtis, 3);
-        System.out.println("Ingredientai, kuriu kaina yra mažesnė už x");
-
-        System.out.println("------------------");
-
-        PapildomosFunkcijos.isvestiIngredientuListaEilutemis(igredientuKaina);
-
-        //---------------------------------------
-
-        System.out.println("------------------");
-        double vidurkis = VeiksmaiSuDuomenuBaze.grazintiIngredientoKainuVidurki(jungtis);
-        System.out.println("Ingredientu kainu vidurkis yra = " + vidurkis);
-
-        System.out.println("------------------");
-
-        //------------------------------------
-
-        System.out.println("------------------");
-        ArrayList<Ingredientai> igredientuTop = VeiksmaiSuDuomenuBaze.grazintiTopIngredientus(jungtis, 5);
-        System.out.println("Ingredientai, kuriu kaina yra didzausia x");
-
-        System.out.println("------------------");
-
-        PapildomosFunkcijos.isvestiIngredientuListaEilutemis(igredientuTop);
-
-        //-----------------------------------
-        System.out.println("------------------");
-        ArrayList<Ingredientai> receptoIngridientai = VeiksmaiSuDuomenuBaze.grazintiReceptoIngredientus(jungtis, 6);
-        System.out.println("Ingredientai, ieinantys i n-taji recepta ");
-
-        System.out.println("------------------");
-
-        PapildomosFunkcijos.isvestiIngredientuListaEilutemis(receptoIngridientai);
-
-        //Ivedimas per konsole----------------
-
-        System.out.println("------------------");
-
-        Receptai naujasReceptas = KonsolesVeiksmai.ivestiReceptusPerKonsole();
-        VeiksmaiSuDuomenuBaze.pridetiRecepta(jungtis, naujasReceptas);
-
-
+            case 3:
+                double vidurkis = VeiksmaiSuDuomenuBaze.grazintiIngredientoKainuVidurki(jungtis);
+                System.out.println("Ingredientu kainu vidurkis yra = " + vidurkis);
+                break;
+            case 4:
+                double mazesneIngredientuKaina = KonsolesVeiksmai.nuskaitytiIgredientusPigesniusUz();
+                ArrayList<Ingredientai> igredientuKaina = VeiksmaiSuDuomenuBaze.grazintiIngredientoKaina(jungtis, mazesneIngredientuKaina);
+                PapildomosFunkcijos.isvestiIngredientuListaEilutemis(igredientuKaina);
+                break;
+            case 5:
+                int ingredientuTopai = KonsolesVeiksmai.nuskaitytiBrangiausiuIgredientuTop();
+                ArrayList<Ingredientai> igredientuTop = VeiksmaiSuDuomenuBaze.grazintiTopIngredientus(jungtis, ingredientuTopai);
+                PapildomosFunkcijos.isvestiIngredientuListaEilutemis(igredientuTop);
+                break;
+            case 6:
+                int visiReceptoIgredientai = KonsolesVeiksmai.nuskaitytiNtojoReceptoIngredientus();
+                ArrayList<Ingredientai> receptoIngredientai = VeiksmaiSuDuomenuBaze.grazintiReceptoIngredientus(jungtis, visiReceptoIgredientai);
+                PapildomosFunkcijos.isvestiIngredientuListaEilutemis(receptoIngredientai);
+                break;
+            case 7:
+                Receptai naujasReceptas = KonsolesVeiksmai.ivestiReceptusPerKonsole();
+                VeiksmaiSuDuomenuBaze.pridetiRecepta(jungtis, naujasReceptas);
+                break;
+            case 8:
+                double brangesniReceptai = KonsolesVeiksmai.nuskaitytiReceptusBrangesniusUz();
+                ArrayList<Receptai> receptuKaina = VeiksmaiSuDuomenuBaze.grazintiKaina(jungtis, brangesniReceptai);
+                PapildomosFunkcijos.isvestiReceptuListaEilutemis(receptuKaina);
+        }
     }
 }
+
+
--- src/main/java/pagalbiniai/KonsolesVeiksmai.java
+++ src/main/java/pagalbiniai/KonsolesVeiksmai.java
@@ -7,14 +7,16 @@ import java.util.Scanner;
 public class KonsolesVeiksmai {
 
     private static final Scanner skaitytuvas = new Scanner(System.in);
-    public static Receptai ivestiReceptusPerKonsole(){
+
+    public static Receptai ivestiReceptusPerKonsole() {
 
         System.out.println("Iveskite recepto pavadinima");
+        skaitytuvas.nextLine();
         String pavadinimas = skaitytuvas.nextLine();
         System.out.println("Iveskite recepto kaina");
         double kaina = skaitytuvas.nextDouble();
-        System.out.println("Iveskite nurodymus");
         skaitytuvas.nextLine();
+        System.out.println("Iveskite nurodymus");
         String nurodymai = skaitytuvas.nextLine();
 
         System.out.println("Nurodymai = " + nurodymai);
@@ -24,4 +26,44 @@ public class KonsolesVeiksmai {
         return new Receptai(pavadinimas, kaina, nurodymai);
     }
 
+    public static int meniuPasirinkimai() {
+        System.out.println("Meniu pasirinkimai");
+        System.out.println("1 - Išvesti visus receptus ");
+        System.out.println("2 - Išvesti visus igredientus ");
+        System.out.println("3 - Išvesti visų ingredientų kainų vidurkį");
+        System.out.println("4 - Ingredientai , pigesni už ivedama kaina");
+        System.out.println("5 - Kelis brangiausius? ");
+        System.out.println("6 - Kelinto recepto?");
+        System.out.println("7 - Ivesti recepto duomenis");
+        System.out.println("8 - Receptai, brangesni uz ivedama kaina");
+
+        return skaitytuvas.nextInt();
+
+    }
+
+    public static double nuskaitytiIgredientusPigesniusUz() {
+        System.out.println("Iveskite, uz kiek pigesnius ingredientus rodyti");
+
+        return skaitytuvas.nextDouble();
+    }
+
+    public static int nuskaitytiBrangiausiuIgredientuTop() {
+        System.out.println("Iveskite, kelis brangieusius ingredientus rodyti");
+
+        return skaitytuvas.nextInt();
+    }
+
+    public static int nuskaitytiNtojoReceptoIngredientus() {
+        System.out.println("Iveskite, recepto id, kurio ingredientus rodyti");
+
+        return skaitytuvas.nextInt();
+    }
+
+    public static double nuskaitytiReceptusBrangesniusUz() {
+        System.out.println("Iveskite, uz kiek brangesnius receptus  rodyti");
+
+        return skaitytuvas.nextDouble();
+
+
+    }
 }
