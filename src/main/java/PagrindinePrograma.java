dd05e7b758796d6a5401641323570b7419e61703
saulgiedr1 <saulgiedr@gmail.com>
2022-07-29 10:52:15 +0300

    prideta recepto ivedimas per konsole

--- src/main/java/PagrindinePrograma.java
+++ src/main/java/PagrindinePrograma.java
@@ -11,25 +11,23 @@ import java.util.ArrayList;
 
 public class PagrindinePrograma {
     public static void main(String[] args) {
-        Connection jungtis= null;
+        Connection jungtis = null;
         try {
 
             jungtis = VeiksmaiSuDuomenuBaze.prisijungtiPrieDuombenuBazesReceptaiDb();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Nepavyko prisijungti prie duomenų bazės.");
         }
         try {
-            ArrayList<Receptai> visiReceptai=VeiksmaiSuDuomenuBaze.grazintiVisusReceptus(jungtis);
+            ArrayList<Receptai> visiReceptai = VeiksmaiSuDuomenuBaze.grazintiVisusReceptus(jungtis);
 
             PapildomosFunkcijos.isvestiReceptuListaEilutemis(visiReceptai);
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         System.out.println("------------------");
-        ArrayList<Receptai> receptuKaina=VeiksmaiSuDuomenuBaze.grazintiKaina(jungtis, 20);
+        ArrayList<Receptai> receptuKaina = VeiksmaiSuDuomenuBaze.grazintiKaina(jungtis, 20);
 
         System.out.println("Receptai, kuriu kaina yra didesnė už x");
 
@@ -39,23 +37,22 @@ public class PagrindinePrograma {
 
         System.out.println("------------------");
 
-      // Receptai pridetiRecepta = new Receptai("Bestrogenas",25.69,"Iškepti");
-      //  VeiksmaiSuDuomenuBaze.pridetiRecepta(jungtis, pridetiRecepta);
+       // Receptai pridetiRecepta = new Receptai("Bestrogenas", 25.69, "Iškepti");
+       // VeiksmaiSuDuomenuBaze.pridetiRecepta(jungtis, pridetiRecepta);
 
 
         //---------------------------------------------------------
 
         try {
-            ArrayList<Ingredientai> visiIngredientai=VeiksmaiSuDuomenuBaze.grazintiVisusIngredientus(jungtis);
+            ArrayList<Ingredientai> visiIngredientai = VeiksmaiSuDuomenuBaze.grazintiVisusIngredientus(jungtis);
 
             PapildomosFunkcijos.isvestiIngredientuListaEilutemis(visiIngredientai);
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
         System.out.println("------------------");
-        ArrayList<Ingredientai> igredientuKaina=VeiksmaiSuDuomenuBaze.grazintiIngredientoKaina(jungtis, 3);
+        ArrayList<Ingredientai> igredientuKaina = VeiksmaiSuDuomenuBaze.grazintiIngredientoKaina(jungtis, 3);
 
         System.out.println("Ingredientai, kuriu kaina yra mažesnė už x");
 
@@ -66,7 +63,7 @@ public class PagrindinePrograma {
         //---------------------------------------
 
         System.out.println("------------------");
-        double vidurkis=VeiksmaiSuDuomenuBaze.grazintiIngredientoKainuVidurki(jungtis);
+        double vidurkis = VeiksmaiSuDuomenuBaze.grazintiIngredientoKainuVidurki(jungtis);
 
         System.out.println("Ingredientu kainu vidurkis yra = " + vidurkis);
 
@@ -75,7 +72,7 @@ public class PagrindinePrograma {
         //------------------------------------
 
         System.out.println("------------------");
-        ArrayList<Ingredientai> igredientuTop=VeiksmaiSuDuomenuBaze.grazintiTopIngredientus(jungtis, 5);
+        ArrayList<Ingredientai> igredientuTop = VeiksmaiSuDuomenuBaze.grazintiTopIngredientus(jungtis, 5);
 
         System.out.println("Ingredientai, kuriu kaina yra didzausia x");
 
@@ -85,13 +82,20 @@ public class PagrindinePrograma {
 
         //-----------------------------------
         System.out.println("------------------");
-        ArrayList<Ingredientai> receptoIngridientai=VeiksmaiSuDuomenuBaze.grazintiReceptoIngredientus(jungtis, 6);
+        ArrayList<Ingredientai> receptoIngridientai = VeiksmaiSuDuomenuBaze.grazintiReceptoIngredientus(jungtis, 6);
 
         System.out.println("Ingredientai, ieinantys i n-taji recepta ");
 
         System.out.println("------------------");
 
         PapildomosFunkcijos.isvestiIngredientuListaEilutemis(receptoIngridientai);
+
+        //Ivedimas per konsole----------------
+
+        System.out.println("------------------");
+
+        Receptai naujasReceptas = PapildomosFunkcijos.nuskaitytiReceptusIsKonsoles();
+        VeiksmaiSuDuomenuBaze.pridetiRecepta(jungtis,naujasReceptas);
 
 
     }
--- src/main/java/pagalbiniai/PapildomosFunkcijos.java
+++ src/main/java/pagalbiniai/PapildomosFunkcijos.java
@@ -4,6 +4,7 @@ import entities.Ingredientai;
 import entities.Receptai;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PapildomosFunkcijos {
     public PapildomosFunkcijos() {
@@ -20,6 +21,26 @@ public class PapildomosFunkcijos {
         for (Ingredientai ingredientai : ingredientas) {
             System.out.println("ingredientai = " + ingredientai);
         }
+    }
+
+    //----------------------------------------
+    public static Receptai nuskaitytiReceptusIsKonsoles(){
+        Scanner skaitytuvas = new Scanner(System.in);
+        System.out.println("Iveskite recepto pavadinima");
+        String pavadinimas = skaitytuvas.nextLine();
+        System.out.println("Iveskite recepto kaina");
+        double kaina = skaitytuvas.nextDouble();
+        System.out.println("Iveskite nurodymus");
+        skaitytuvas.nextLine();
+        String nurodymai = skaitytuvas.nextLine();
+
+
+        System.out.println("nurodymai = " + nurodymai);
+        System.out.println("kaina = " + kaina);
+        System.out.println("pavadinimas = " + pavadinimas);
+
+        return new Receptai(pavadinimas, kaina, nurodymai);
+
     }
 
 
